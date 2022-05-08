f6892b1380accfe4dfc513109943c17217c0c787
steelvirus <mlatussek@yahoo.de>
2022-05-08 15:08:44 +0200

    Update example.js routes

--- src/routes/example.js
+++ src/routes/example.js
@@ -1,19 +1,25 @@
 import {ExampleController} from "../js/View/Controllers/ExampleController";
+import {ExampleMiddleware} from "../js/Routing/Middleware/ExampleMiddleware";
 
 /**
  * @param {Routes} routes
  * @returns {Routes}
  */
 export default function (routes) {
-    routes.add('/', [ExampleController, 'welcome']).name('example');
-    routes.add('/tests/:prop1', [ExampleController, 'tests']).name('tests');
+    routes.middleware(ExampleMiddleware).group((routes) => {
 
-    routes.prefix('/subdomain').domain('test.latus.local').group((routes) => {
+        routes.add('/', [ExampleController, 'welcome']).name('example');
+        routes.add('/tests/:prop1', [ExampleController, 'tests']).name('tests');
 
-        routes.add('/only', [ExampleController, 'subdomain']).name('subdomain');
+        routes.prefix('/subdomain').domain('test.latus.local').group((routes) => {
+
+            routes.add('/only', [ExampleController, 'subdomain']).name('subdomain');
+
+            return routes;
+        });
 
         return routes;
-    });
+    })
 
     return routes;
 }
